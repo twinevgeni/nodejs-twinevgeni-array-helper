85d8ac499964d440e53f86f9910f448bc099bc48
Ижболдин Евгений Юрьевич <eizhboldin@vtb.ru>
2023-08-28 00:55:26 +0300

    v 1.0.1 arrayIsEmpty & arrayIsNotEmpty functions

--- src/twinevgeni-array-helper.js
+++ src/twinevgeni-array-helper.js
@@ -1,9 +1,21 @@
 module.exports = function () {
+
+    /**
+     * Check if array contains needle
+     * @param needle
+     * @param array
+     * @returns {boolean}
+     */
     function arrayContains(needle, array) {
         if (!array) return false;
         return (array.indexOf(needle) > -1);
     }
 
+    /**
+     * trim all elements in array
+     * @param array
+     * @returns {*}
+     */
     function arrayTrimAll(array) {
         for (let i = 0; i < array.length; i++) {
             array[i] = array[i].trim();
@@ -12,8 +24,28 @@ module.exports = function () {
         return array;
     }
 
+    /**
+     * check that array is not empty
+     * @param array
+     * @returns {boolean}
+     */
+    function arrayIsNotEmpty(array) {
+        return array !== null && array !== 'undefined' && array.length > 0;
+    }
+
+    /**
+     * check that array is empty
+     * @param array
+     * @returns {boolean}
+     */
+    function arrayIsEmpty(array) {
+        return !arrayIsNotEmpty(array);
+    }
+
     return {
         arrayContains: arrayContains,
-        arrayTrimAll: arrayTrimAll
+        arrayTrimAll: arrayTrimAll,
+        arrayIsEmpty: arrayIsEmpty,
+        arrayIsNotEmpty: arrayIsNotEmpty
     }
 }();
